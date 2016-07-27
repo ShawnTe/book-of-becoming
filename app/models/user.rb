e7ed7f6adfeb18a83f3c3059b5e8482a8bf2e0d4
Shawn Tuttle <email": null>
2016-07-26 19:00:53 -0700

    Small changes for debugging

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,21 @@
 class User < ActiveRecord::Base
-  include BCrypt
+  # include BCrypt
 
   has_many :posts
   has_many :tags
 
   # validates :password_digest, length: { minimum: 8 }
-  validates_presence_of :email, :first_name
+  validates_presence_of :first_name
   has_secure_password
 
-
 # Replaced with has_secure_password
-  def password
-    @password ||= Password.new(password_digest)
-  end
+  # def password
+  #   @password ||= Password.new(password_digest)
+  # end
 
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_digest = @password
-  end
+  # def password=(new_password)
+  #   @password = Password.create(new_password)
+  #   self.password_digest = @password
+  # end
+
 end
