414226b541146b947ff96c1629885c708b108d1f
Shawn Tuttle <email": null>
2016-07-26 18:59:44 -0700

    Add notes ajax toggle, change name on toggle button

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,38 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  createNote();
+  toggleField();
 });
+
+function createNote() {
+  $("#create-post").on('submit', function(event) {
+    console.log($(this));
+    console.log(event);
+    event.preventDefault();
+    var formData = $('create-post').serialize()
+    $.ajax ({
+      url: "/posts",
+      method: "post",
+      data: formData
+    }).done(function() {
+      console.log(serverResponse);
+      $("#full-post-list").append("<li>server data</li>")
+    })
+  })
+}
+
+function toggleField(){
+  $("#notes-tgl-btn").on('click', function(event) {
+    event.preventDefault();
+
+    $("#notes-tgl-field").toggle();
+    $(this).val(function(i, text){
+      return text === "Show notes" ? "Hide notes" : "Show notes";
+    })
+  })
+}
+
+
+
+
